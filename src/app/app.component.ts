fcf294d5218883fdb03bfe33047a6a670f969e56
cristhian.velasquez <cristhian.velasquez@e-ecapture.io>
2024-07-11 19:39:37 -0500

    feat: added module about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NgIf} from "@angular/common";
+declare var WOW: any;
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,8 @@ import {NgIf} from "@angular/common";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    new WOW().init();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./modules/home/home.component')
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./modules/about/about.component')
   }
 ];
